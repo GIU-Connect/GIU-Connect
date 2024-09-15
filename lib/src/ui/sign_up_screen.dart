5c30ff35e283e2bf38b8473e3b2ffffeda881175
momenashrafff <momenashraf742@gmail.com>
2024-09-15 20:13:56 +0300

    Cleaned the Sign Up Screen

--- lib/src/ui/sign_up_screen.dart
+++ lib/src/ui/sign_up_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:group_changing_app/src/services/auth_service.dart';
+import 'package:group_changing_app/src/ui/home_page_screen.dart';
 import 'package:group_changing_app/src/ui/sign_in_screen.dart';
 import 'package:group_changing_app/src/widgets/my_button.dart';
 import 'package:group_changing_app/src/widgets/my_textfield.dart';
@@ -8,162 +9,252 @@ class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
 
   @override
-  _SignUpScreenState createState() => _SignUpScreenState();
+  SignUpScreenState createState() => SignUpScreenState();
 }
 
-class _SignUpScreenState extends State<SignUpScreen> {
-  final _emailController = TextEditingController();
-  final _passwordController = TextEditingController();
-  final _confirmPasswordController = TextEditingController();
-  final _phoneNumberController = TextEditingController();
-  final _universityIdController = TextEditingController();
-  final _currentTutorialController = TextEditingController();
-  final _fullNameController = TextEditingController();
-  String semester = '1';
+class SignUpScreenState extends State<SignUpScreen> {
+  final _formKey = GlobalKey<FormState>();
 
-  String major = 'CS';
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController = TextEditingController();
+  final TextEditingController _phoneNumberController = TextEditingController();
+  final TextEditingController _universityIdController = TextEditingController();
+  final TextEditingController _currentTutorialController = TextEditingController();
+  final TextEditingController _fullNameController = TextEditingController();
 
-  final _authService = AuthService();
+  String? _semester;
+  String? _major;
+  bool _isLoading = true;
 
-  void _signUp() async {
-    final email = _emailController.text;
-    final password = _passwordController.text;
-    final confirmPassword = _confirmPasswordController.text;
-    final phoneNumber = _phoneNumberController.text;
-    final universityId = _universityIdController.text;
-    final currentTutorial = _currentTutorialController.text;
-    final name = _fullNameController.text;
-    final semester = this.semester;
+  final AuthService _authService = AuthService();
 
-    if (password != confirmPassword) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Passwords do not match')),
-      );
-      return;
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _checkIfLoggedIn();
+    });
+  }
+
+  Future<void> _checkIfLoggedIn() async {
+    if (_authService.currentUser != null && _authService.currentUser!.emailVerified) {
+      if (mounted) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const HomePageScreen()),
+        );
+      }
+    } else if (_authService.currentUser != null && !_authService.currentUser!.emailVerified) {
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Please verify your email')),
+        );
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const SignInScreen()),
+        );
+      }
+    } else {
+      if (mounted) {
+        setState(() {
+          _isLoading = false;
+        });
+      }
     }
+  }
 
-    try {
-      await _authService.signUp(
-        email: email,
-        password: password,
-        confirmPassword: confirmPassword,
-        phoneNumber: phoneNumber,
-        universityId: universityId,
-        major: major,
-        currentTutorial: currentTutorial,
-        name :name,
-        semester: semester,
-      );
+  Future<void> _signUp() async {
+    if (_formKey.currentState?.validate() ?? false) {
+      final email = _emailController.text.trim();
+      final password = _passwordController.text.trim();
+      final confirmPassword = _confirmPasswordController.text.trim();
+      final phoneNumber = _phoneNumberController.text.trim();
+      final universityId = _universityIdController.text.trim();
+      final currentTutorial = _currentTutorialController.text.trim();
+      final fullName = _fullNameController.text.trim();
 
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => const SignInScreen()),
-      );
-    } catch (e) {
-      print(e);
+      if (password != confirmPassword) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Passwords do not match')),
+        );
+        return;
+      }
+
+      try {
+        await _authService.signUp(
+          email: email,
+          password: password,
+          confirmPassword: confirmPassword,
+          phoneNumber: phoneNumber,
+          universityId: universityId,
+          major: _major ?? '',
+          currentTutorial: currentTutorial,
+          name: fullName,
+          semester: _semester ?? '',
+        );
+        if (mounted) {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => const SignInScreen()),
+          );
+        }
+      } catch (e) {
+        if (mounted) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Sign up failed. Please try again.')),
+          );
+        }
+      }
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Sign Up')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            MyTextField(
-              controller: _fullNameController,
-              hintText: 'Full Name',
-              obscureText: false,
+    return _isLoading
+        ? const Scaffold(body: Center(child: CircularProgressIndicator()))
+        : Scaffold(
+            appBar: AppBar(title: const Text('Sign Up')),
+            body: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Form(
+                key: _formKey,
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      MyTextField(
+                        controller: _fullNameController,
+                        hintText: 'Full Name',
+                        obscureText: false,
+                        validator: (value) => value == null || value.isEmpty ? 'Please enter your full name' : null,
+                      ),
+                      MyTextField(
+                        controller: _emailController,
+                        hintText: 'Email',
+                        obscureText: false,
+                        validator: (value) => value == null || value.isEmpty ? 'Please enter your email' : null,
+                      ),
+                      MyTextField(
+                        controller: _passwordController,
+                        hintText: 'Password',
+                        obscureText: true,
+                        validator: (value) => value == null || value.isEmpty ? 'Please enter your password' : null,
+                      ),
+                      MyTextField(
+                        controller: _confirmPasswordController,
+                        hintText: 'Confirm Password',
+                        obscureText: true,
+                        validator: (value) => value == null || value.isEmpty ? 'Please confirm your password' : null,
+                      ),
+                      MyTextField(
+                        controller: _phoneNumberController,
+                        hintText: 'Phone Number',
+                        obscureText: false,
+                        validator: (value) => value == null || value.isEmpty ? 'Please enter your phone number' : null,
+                      ),
+                      MyTextField(
+                        controller: _universityIdController,
+                        hintText: 'University ID',
+                        obscureText: false,
+                        validator: (value) => value == null || value.isEmpty ? 'Please enter your university ID' : null,
+                      ),
+                      MyTextField(
+                        controller: _currentTutorialController,
+                        hintText: 'Current Tutorial',
+                        obscureText: false,
+                        validator: (value) =>
+                            value == null || value.isEmpty ? 'Please enter your current tutorial' : null,
+                      ),
+                      _buildDropdown(
+                        'Choose Major',
+                        _major,
+                        [
+                          'Informatics and Computer Science',
+                          'Business Administration',
+                          'Engineering',
+                          'Pharmaceutical Engineering',
+                          'Business Informatics',
+                          'Architecture',
+                        ],
+                        (value) {
+                          setState(() {
+                            _major = value;
+                          });
+                        },
+                      ),
+                      _buildDropdown(
+                        'Choose Semester',
+                        _semester,
+                        List.generate(8, (index) => (index + 1).toString()),
+                        (value) {
+                          setState(() {
+                            _semester = value;
+                          });
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      MyButton(onTap: _signUp, buttonName: 'Sign Up'),
+                      const SizedBox(height: 20),
+                      MyButton(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => const SignInScreen()),
+                          );
+                        },
+                        buttonName: 'Already have an account? Sign In',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
-          
-            MyTextField(
-              controller: _emailController,
-              hintText: 'Email',
-              obscureText: false,
-            ),
-            MyTextField(
-              controller: _passwordController,
-              hintText: 'Password',
-              obscureText: true,
-            ),
-            MyTextField(
-              controller: _confirmPasswordController,
-              hintText: 'Confirm Password',
-              obscureText: true,
-            ),
-            MyTextField(
-              controller: _phoneNumberController,
-              hintText: 'Phone Number',
-              obscureText: false,
-            ),
-            MyTextField(
-              controller: _universityIdController,
-              hintText: 'University ID',
-              obscureText: false,
-            ),
-            MyTextField(
-              controller: _currentTutorialController,
-              hintText: 'Current Tutorial',
-              obscureText: false,
-            ),
-            // Dropdown for Major
-            DropdownButtonFormField<String>(
-              decoration: const InputDecoration(hintText: 'Major'),
-              value: major,
-              items: [
-                'CS',
-                'BA',
-                'Engineering',
-                'Pharmaceutical Engineering',
-                'BI',
-                'Architecture'
-              ]
-                  .map((major) => DropdownMenuItem(
-                        value: major,
-                        child: Text(major),
-                      ))
-                  .toList(),
-              onChanged: (value) {
-                setState(() {
-                  major = value!;
-                });
-              },
-            ),
-            // Dropdown for Semester
-            DropdownButtonFormField<String>(
-              decoration: const InputDecoration(hintText: 'Semester'),
-              value: semester,
-              items: List.generate(8, (index) => (index + 1).toString())
-                  .map((semester) => DropdownMenuItem(
-                        value: semester,
-                        child: Text(semester),
-                      ))
-                  .toList(),
-              onChanged: (value) {
-                setState(() {
-                  semester = value!;
-                });
-              },
-            ),
-            const SizedBox(height: 20),
-            MyButton(onTap: _signUp, buttonName: 'Sign Up'),
-            const SizedBox(height: 20),
-            // Already have an account
-            MyButton(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const SignInScreen()),
-                );
-              },
-              buttonName: 'Already have an account? Sign In',
-            ),
-          ],
+          );
+  }
+
+  Widget _buildDropdown(
+    String hint,
+    String? value,
+    List<String> items,
+    ValueChanged<String?> onChanged,
+  ) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: DropdownButtonFormField<String>(
+        decoration: InputDecoration(
+          enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey.shade400),
+          ),
+          fillColor: Colors.grey.shade200,
+          filled: true,
+          hintText: hint,
+          hintStyle: TextStyle(color: Colors.grey[500]),
         ),
+        value: value,
+        items: items
+            .map((item) => DropdownMenuItem<String>(
+                  value: item,
+                  child: Text(item),
+                ))
+            .toList(),
+        onChanged: onChanged,
+        validator: (value) => value == null ? 'Please select $hint' : null,
       ),
     );
   }
-  
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    _phoneNumberController.dispose();
+    _universityIdController.dispose();
+    _currentTutorialController.dispose();
+    _fullNameController.dispose();
+    super.dispose();
+  }
 }
--- lib/src/widgets/my_textfield.dart
+++ lib/src/widgets/my_textfield.dart
@@ -1,35 +1,39 @@
 import 'package:flutter/material.dart';
 
 class MyTextField extends StatelessWidget {
-  final controller;
+  final TextEditingController controller;
   final String hintText;
   final bool obscureText;
+  final String? Function(String?)? validator;
 
   const MyTextField({
     super.key,
     required this.controller,
     required this.hintText,
     required this.obscureText,
+    this.validator,
   });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 25.0),
-      child: TextField(
+      child: TextFormField(
         controller: controller,
         obscureText: obscureText,
         decoration: InputDecoration(
-            enabledBorder: const OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.white),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.grey.shade400),
-            ),
-            fillColor: Colors.grey.shade200,
-            filled: true,
-            hintText: hintText,
-            hintStyle: TextStyle(color: Colors.grey[500])),
+          enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey.shade400),
+          ),
+          fillColor: Colors.grey.shade200,
+          filled: true,
+          hintText: hintText,
+          hintStyle: TextStyle(color: Colors.grey[500]),
+        ),
+        validator: validator,
       ),
     );
   }
