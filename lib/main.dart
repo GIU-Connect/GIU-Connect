be18b029ea46cc98595a28e0135f63a2419841d9
momenashrafff <momenashraf742@gmail.com>
2024-09-20 12:30:14 +0300

    fixed IOS version

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
+  print('Firebase initialized');
   runApp(const MyApp());
 }
 
